fe6896a8613830485cd4bb986c95d9fe5a82a7a9
Wu Ze <zwubn@connect.ust.hk>
2023-03-23 19:59:32 +0000

    done

--- printvars.c
+++ printvars.c
@@ -9,5 +9,26 @@ char *custom_env[] = {"USER=Pikachu", "PATH=/tmp", NULL};
 int main(void)
 {
     // TODO
+    pid_t pid1=fork();
+    if(pid1==0)
+    {
+        printf("Running child process PID: %d\n",getpid());
+        char *arg[]={"echoall","Bandicoot","Pacman",NULL};
+        execle("/home/zwubn/-CSCE313-SP23-Kebo-quiz-2-2./echoall",arg[0],arg[1],arg[2],NULL,custom_env);
+    }
+    else{
+        printf("Running parent process PID: %d\n",getpid());
+        printf("\n");
+        wait(NULL);
+        pid_t pid2=fork();
+        if(pid2==0)
+        {
+            printf("Running child process PID: %d\n",getpid());
+            
+            execlp("/home/zwubn/-CSCE313-SP23-Kebo-quiz-2-2./echoall","Spyro",NULL);
+
+        }
+        wait(NULL);
+    }
     exit(0);
 }
